7b2106379ec7b538d833e8ecb9c732c8bed63004
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-04 11:31:34 -0800

    write test for Mercury years past life expectancy.

--- __tests__/galactic-age.tests.js
+++ __tests__/galactic-age.tests.js
@@ -24,4 +24,10 @@ describe('User', () => {
     expect(user.lifeExpectancy).toEqual(204);
   });
 
+  test('should add a yearsPast property with a positive number if user age is higher than lifeExpectancy', () => {
+    let user = new User(85,79);
+    user.mercuryAge();
+    expect(user.yearsPast).toEqual(24);
+  });
+
 });
