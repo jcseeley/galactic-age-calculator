662556cab51a26d8e1282f72a2a01aa286687768
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-06 10:05:02 -0800

    reword user object test to include yearsPast property.

--- __tests__/galactic-age.tests.js
+++ __tests__/galactic-age.tests.js
@@ -8,9 +8,10 @@ describe('User', () => {
     user = new User(30,79);
   });
 
-  test('should correctly create a user object with age and lifeExpectancy', () => {
+  test('should correctly create a user object with age, lifeExpectancy, and yearsPast properties', () => {
     expect(user.age).toEqual(30);
-    expect(user.lifeExpectancy).toEqual(79); 
+    expect(user.lifeExpectancy).toEqual(79);
+    expect(user.yearsPast).toEqual(0);
   });
 
   test('should change user age to Mercury years', () => {
